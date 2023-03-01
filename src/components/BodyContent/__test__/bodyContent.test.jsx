b218329145ea1a4dae981f51557e98b621bdb0b8
Kamaleshen S <kamaleshen.s@gmail.com>
2023-03-01 18:05:12 +0530

    chore: fixed unwanted imports

--- src/components/BodyContent/__test__/bodyContent.test.jsx
+++ src/components/BodyContent/__test__/bodyContent.test.jsx
@@ -1,6 +1,5 @@
-import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import BodyContent from '../bodyContent';
 import axios from 'axios';
 
--- src/components/FilterComponent/__test__/filterComponent.test.jsx
+++ src/components/FilterComponent/__test__/filterComponent.test.jsx
@@ -1,6 +1,5 @@
-import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import FilterComponent from '../filterComponent';
 
 jest.mock('react-router-dom');
